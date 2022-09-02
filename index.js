32f6e41092619b8778b0afad04ae0d147a82490c
Bsayavong <bksayavong@gmail.com>
2022-09-02 16:56:28 -0700

    line 2-3 index.js, pre tutor sesh

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: '',
+    message: '',
+    name: '',
+
+    // repeat?? How many questions?
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    //fs.writeFile() ?????
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //inquirer.????
+        //then??
+}
 
 // Function call to initialize app
 init();
